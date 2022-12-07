e125533a37b9027cf90f7e27a2966e174bf637ff
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-12-07 14:17:41 +0100

    Iterate once

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -5,60 +5,58 @@ enum Command {
     File(usize),
 }
 
-fn parse_input(input: &'static str) -> Vec<Command> {
-    input
-        .lines()
-        .filter_map(|line| {
-            let mut parts = line.split_whitespace();
-            match (parts.next(), parts.next()) {
-                (Some("$"), Some("cd")) => {
-                    if let Some(dirname) = parts.next() {
-                        if dirname == ".." {
-                            return Some(Command::DirectoryUp);
-                        } else {
-                            return Some(Command::Directory(dirname));
-                        }
-                    }
-                    None
+fn to_command(input_line: &'static str) -> Option<Command> {
+    let mut parts = input_line.split_whitespace();
+    match (parts.next(), parts.next()) {
+        (Some("$"), Some("cd")) => {
+            if let Some(dirname) = parts.next() {
+                if dirname == ".." {
+                    return Some(Command::DirectoryUp);
+                } else {
+                    return Some(Command::Directory(dirname));
                 }
-                (Some(maybe_num), _) => {
-                    if let Ok(num) = maybe_num.parse() {
-                        return Some(Command::File(num));
-                    }
-                    None
-                }
-                _ => None,
             }
-        })
-        .collect()
+            None
+        }
+        (Some(maybe_num), _) => {
+            if let Ok(num) = maybe_num.parse() {
+                return Some(Command::File(num));
+            }
+            None
+        }
+        _ => None,
+    }
 }
 
-fn find_sizes(commands: &[Command]) -> HashMap<Vec<&str>, usize> {
+fn find_sizes(input: &'static str) -> HashMap<Vec<&str>, usize> {
     let mut path: Vec<&str> = Vec::new();
 
-    commands.iter().fold(HashMap::new(), |mut sizes, command| {
-        match command {
-            Command::DirectoryUp => {
-                path.pop();
+    input
+        .lines()
+        .filter_map(to_command)
+        .fold(HashMap::new(), |mut sizes, command| {
+            match command {
+                Command::DirectoryUp => {
+                    path.pop();
+                }
+                Command::Directory(dir) => {
+                    path.push(dir);
+                }
+                Command::File(size) => {
+                    (0..path.len())
+                        .map(|i| path[0..=i].to_vec())
+                        .for_each(|subpath| {
+                            *sizes.entry(subpath).or_insert(0) += size;
+                        });
+                }
             }
-            Command::Directory(dir) => {
-                path.push(dir);
-            }
-            Command::File(size) => {
-                (0..path.len())
-                    .map(|i| path[0..=i].to_vec())
-                    .for_each(|subpath| {
-                        *sizes.entry(subpath).or_insert(0) += size;
-                    });
-            }
-        }
-        sizes
-    })
+            sizes
+        })
 }
 
 fn main() {
-    let commands = parse_input(include_str!("../input.txt"));
-    let sizes = find_sizes(&commands);
+    let input = include_str!("../input.txt");
+    let sizes = find_sizes(input);
 
     let p1: usize = sizes.values().filter(|&&v| v <= 100000).sum();
 
