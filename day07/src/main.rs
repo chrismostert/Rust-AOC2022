d87adce2237edae57c27c7d3e428213355ffb58a
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-12-07 15:20:48 +0100

    Shorten day 7

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,57 +1,30 @@
 use std::collections::HashMap;
-enum Command {
-    DirectoryUp,
-    Directory(&'static str),
-    File(usize),
-}
 
-fn to_command(input_line: &'static str) -> Option<Command> {
-    let mut parts = input_line.split_whitespace();
-    match (parts.next(), parts.next()) {
-        (Some("$"), Some("cd")) => {
-            if let Some(dirname) = parts.next() {
-                if dirname == ".." {
-                    return Some(Command::DirectoryUp);
-                } else {
-                    return Some(Command::Directory(dirname));
-                }
-            }
-            None
-        }
-        (Some(maybe_num), _) => {
-            if let Ok(num) = maybe_num.parse() {
-                return Some(Command::File(num));
-            }
-            None
-        }
-        _ => None,
-    }
-}
-
-fn find_sizes(input: &'static str) -> HashMap<Vec<&str>, usize> {
+fn find_sizes(input: &str) -> HashMap<Vec<&str>, usize> {
     let mut path: Vec<&str> = Vec::new();
 
-    input
-        .lines()
-        .filter_map(to_command)
-        .fold(HashMap::new(), |mut sizes, command| {
-            match command {
-                Command::DirectoryUp => {
-                    path.pop();
-                }
-                Command::Directory(dir) => {
-                    path.push(dir);
-                }
-                Command::File(size) => {
-                    (0..path.len())
-                        .map(|i| path[0..=i].to_vec())
-                        .for_each(|subpath| {
-                            *sizes.entry(subpath).or_insert(0) += size;
-                        });
-                }
+    input.lines().fold(HashMap::new(), |mut sizes, line| {
+        let mut parts = line.split_whitespace();
+        match (parts.next(), parts.next(), parts.next()) {
+            (Some("$"), Some("cd"), Some("..")) => {
+                path.pop();
             }
-            sizes
-        })
+            (Some("$"), Some("cd"), Some(dir)) => {
+                path.push(dir);
+            }
+            (Some("$"), _, _) => return sizes,
+            (Some("dir"), _, _) => return sizes,
+            (Some(size), _, _) => {
+                (0..path.len())
+                    .map(|i| path[0..=i].to_vec())
+                    .for_each(|subpath| {
+                        *sizes.entry(subpath).or_insert(0) += size.parse::<usize>().unwrap();
+                    });
+            }
+            _ => return sizes,
+        };
+        sizes
+    })
 }
 
 fn main() {
@@ -59,12 +32,11 @@ fn main() {
     let sizes = find_sizes(input);
 
     let p1: usize = sizes.values().filter(|&&v| v <= 100000).sum();
-
-    let total_size = 70000000;
-    let size_free = total_size - sizes.get(&vec!["/"]).unwrap();
-    let size_needed = 30000000 - size_free;
-
-    let p2 = sizes.values().filter(|&&v| v >= size_needed).min().unwrap();
+    let p2 = sizes
+        .values()
+        .filter(|&&v| v >= sizes[&vec!["/"]] - 40000000)
+        .min()
+        .unwrap();
 
     println!("Part 1: {p1}");
     println!("Part 2: {p2}");
