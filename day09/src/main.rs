11175a1d056b09c9b634f171daa1863bbce87be8
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-12-09 11:54:33 +0100

    Day09

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,43 +1,38 @@
 use std::collections::HashSet;
 
-fn simulate_rope(input: &str, rope_length: usize) -> (usize, usize) {
+fn simulate_rope(input: &str, rope_length: usize) -> Vec<HashSet<(isize, isize)>> {
     let mut rope: Vec<(isize, isize)> = vec![(0, 0); rope_length];
+    input
+        .lines()
+        .fold(vec![HashSet::new(); rope_length], |mut seen, line| {
+            let (dir, amount) = line.split_once(' ').unwrap();
+            for _ in 0..amount.parse().unwrap() {
+                match dir {
+                    "U" => rope[0].1 += 1,
+                    "D" => rope[0].1 -= 1,
+                    "L" => rope[0].0 -= 1,
+                    "R" => rope[0].0 += 1,
+                    _ => unreachable!(),
+                }
 
-    let mut visited_second: HashSet<(isize, isize)> = HashSet::new();
-    let mut visited_tail: HashSet<(isize, isize)> = HashSet::new();
-
-    for line in input.lines() {
-        let (dir, amount) = line.split_once(' ').unwrap();
-
-        for _ in 0..amount.parse().unwrap() {
-            // Move head
-            match dir {
-                "U" => rope[0].1 += 1,
-                "D" => rope[0].1 -= 1,
-                "L" => rope[0].0 -= 1,
-                "R" => rope[0].0 += 1,
-                _ => unreachable!(),
-            }
-
-            for i in 1..rope.len() {
-                if (rope[i - 1].0 - rope[i].0).abs() == 2 || (rope[i - 1].1 - rope[i].1).abs() == 2
-                {
-                    rope[i].0 += (rope[i - 1].0 - rope[i].0).signum();
-                    rope[i].1 += (rope[i - 1].1 - rope[i].1).signum();
+                for i in 1..rope.len() {
+                    if (rope[i - 1].0 - rope[i].0).abs() == 2
+                        || (rope[i - 1].1 - rope[i].1).abs() == 2
+                    {
+                        rope[i].0 += (rope[i - 1].0 - rope[i].0).signum();
+                        rope[i].1 += (rope[i - 1].1 - rope[i].1).signum();
+                    }
+                    seen[i].insert(*rope.get(i).unwrap());
                 }
             }
-
-            visited_second.insert(*rope.get(1).unwrap());
-            visited_tail.insert(*rope.last().unwrap());
-        }
-    }
-    (visited_second.len(), visited_tail.len())
+            seen
+        })
 }
 
 fn main() {
     let input = include_str!("../input.txt");
-    let (p1, p2) = simulate_rope(input, 10);
+    let seen = simulate_rope(input, 10);
 
-    println!("Part 1: {p1}");
-    println!("Part 2: {p2}");
+    println!("Part 1: {}", seen[1].len());
+    println!("Part 2: {}", seen.last().unwrap().len());
 }
