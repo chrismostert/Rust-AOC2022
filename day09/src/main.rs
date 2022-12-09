308cd8552afba70f1aeb9f19d389dcda8588d16e
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-12-09 11:17:01 +0100

    Day09

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,11 +1,10 @@
 use std::collections::HashSet;
 
-fn main() {
-    let input = include_str!("../input.txt");
+fn simulate_rope(input: &str, rope_length: usize) -> (usize, usize) {
+    let mut rope = vec![(0, 0); rope_length];
 
-    let mut visited: HashSet<(isize, isize)> = HashSet::new();
-    let (mut x_h, mut y_h) = (0, 0);
-    let (mut x_t, mut y_t) = (0, 0);
+    let mut visited_second: HashSet<(isize, isize)> = HashSet::new();
+    let mut visited_tail: HashSet<(isize, isize)> = HashSet::new();
 
     for line in input.lines() {
         let (dir, amount) = line.split_once(' ').unwrap();
@@ -13,43 +12,51 @@ fn main() {
         for _ in 0..amount.parse().unwrap() {
             // Move head
             match dir {
-                "U" => y_h += 1,
-                "D" => y_h -= 1,
-                "L" => x_h -= 1,
-                "R" => x_h += 1,
+                "U" => rope[0].1 += 1,
+                "D" => rope[0].1 -= 1,
+                "L" => rope[0].0 -= 1,
+                "R" => rope[0].0 += 1,
                 _ => unreachable!(),
             }
 
-            // Update tail
-            match (x_h - x_t, y_h - y_t) {
-                (0, 2) => y_t += 1,
-                (0, -2) => y_t -= 1,
-                (-2, 0) => x_t -= 1,
-                (2, 0) => x_t += 1,
-                (1, 2) | (2, 1) => {
-                    x_t += 1;
-                    y_t += 1
+            for i in 1..rope.len() {
+                // Update tail
+                match (rope[i - 1].0 - rope[i].0, rope[i - 1].1 - rope[i].1) {
+                    (0, 2) => rope[i].1 += 1,
+                    (0, -2) => rope[i].1 -= 1,
+                    (-2, 0) => rope[i].0 -= 1,
+                    (2, 0) => rope[i].0 += 1,
+                    (1, 2) | (2, 1) | (2, 2) => {
+                        rope[i].0 += 1;
+                        rope[i].1 += 1
+                    }
+                    (1, -2) | (2, -1) | (2, -2) => {
+                        rope[i].0 += 1;
+                        rope[i].1 -= 1;
+                    }
+                    (-1, -2) | (-2, -1) | (-2, -2) => {
+                        rope[i].0 -= 1;
+                        rope[i].1 -= 1;
+                    }
+                    (-1, 2) | (-2, 1) | (-2, 2) => {
+                        rope[i].0 -= 1;
+                        rope[i].1 += 1;
+                    }
+                    _ => {}
                 }
-                (1, -2) | (2, -1) => {
-                    x_t += 1;
-                    y_t -= 1;
-                }
-                (-1, -2) | (-2, -1) => {
-                    x_t -= 1;
-                    y_t -= 1;
-                }
-                (-1, 2) | (-2, 1) => {
-                    x_t -= 1;
-                    y_t += 1;
-                }
-                _ => {}
             }
 
-            // Add tail pos
-            visited.insert((x_t, y_t));
+            visited_second.insert(*rope.get(1).unwrap());
+            visited_tail.insert(*rope.last().unwrap());
         }
     }
+    (visited_second.len(), visited_tail.len())
+}
 
-    let p1 = visited.len();
-    dbg!(p1);
+fn main() {
+    let input = include_str!("../input.txt");
+    let (p1, p2) = simulate_rope(input, 10);
+
+    println!("Part 1: {p1}");
+    println!("Part 2: {p2}");
 }
